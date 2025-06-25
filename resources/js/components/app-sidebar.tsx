00199df5a18f12d6be49342817a1585aaa60f906
Bikas chaudhary <bikas.c@shikhartech.com>
2025-06-25 10:42:31 +0545

    logo added

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,15 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { MenuSquare,FolderCheckIcon, AirVentIcon, ImageIcon, Folder, LayoutGrid } from 'lucide-react';
+import {
+    MenuSquare,
+    FolderCheckIcon,
+    AirVentIcon,
+    ImageIcon,
+    Folder,
+    LayoutGrid,
+    FileQuestionIcon
+} from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -32,6 +40,11 @@ const mainNavItems: NavItem[] = [
         title: 'Posts',
         href: '/posts',
         icon: AirVentIcon,
+    },
+    {
+        title: 'FAQs',
+        href: '/faqs',
+        icon: FileQuestionIcon,
     },
 ];
 
